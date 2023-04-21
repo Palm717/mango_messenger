e212060ce661d8f6f8e2b7e15ec4e0547b61fdc5
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-04-21 10:49:59 -0400

    JD

--- server.js
+++ server.js
@@ -8,13 +8,19 @@ const PORT = process.env.PORT || 3001;
 
 // const listeners = server.listeners("request").slice(0);
 const view_routes = require("./controllers/view_routes");
-const db = require("./db/connection");
+const db = require("./config/connection");
 
 const app = express();
 
 const http = require("http");
 const server = http.createServer(app);
 const io = new Server(server);
+const sessionMiddleware = session({
+  //Required to be used to validate the client cookie matches the session secret
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+});
 
 //Allow the client to send through json
 app.use(express.json());
@@ -37,25 +43,15 @@ app.set("view engine", "hbs");
 app.set("views", "./views");
 
 //Setup the req.session object for our routes
-app.use(
-  session({
-    //Required to be used to validate the client cookie matches the session secret
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+app.use(sessionMiddleware);
 
-//Load all of our routes at the root
 app.use("/", view_routes);
 
-db.sync().then(() => {
-  app.listen(PORT, () => console.log("Server started on port %s", PORT));
-});
+io.engine.use(sessionMiddleware);
 
-// Message appears in the console once a user Logs in
 io.on("connection", (socket) => {
   console.log("A user connected");
+  console.log(socket.request.session);
   socket.on("chat message", (msg) => {
     socket.emit("chat message", msg);
   });
@@ -64,3 +60,13 @@ io.on("connection", (socket) => {
     console.log("A user disconnected");
   });
 });
+
+//Load all of our routes at the root
+
+db.sync().then(() => {
+  server.listen(PORT, () =>
+    console.log(`Server started on http://localhost:${PORT}`)
+  );
+});
+
+// Message appears in the console once a user Logs in
